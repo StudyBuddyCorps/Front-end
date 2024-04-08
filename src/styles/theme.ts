3afbe07bd3f1cd486d8455e4d39ec494f1473b03
Justice <juijeong8324@gmail.com>
2024-04-08 22:34:35 +0900

    feat: Add theme and folder styles
    Sign-off-by: juijeong8324 <juijeong8324gmail.com>

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "styled-components";
+
+// define colors
+const colors = {
+  main: "#FFDA58",
+  subMain: "#586FC5",
+  highlight: "#FF007A",
+  green: "#1497A9",
+  white00: "#ECECEC",
+  white01: "#F5F5F5",
+  white02: "#FFFFFF",
+  black00: "#131211",
+  black01: "#181D24",
+  black02: "#212E42",
+  background: "#4A5058",
+  background2: "#303237",
+};
+
+export type Colors = typeof colors;
+
+const Color: DefaultTheme = {
+  colors,
+};
