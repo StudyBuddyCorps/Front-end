2fdae953eba96e7709688bf43496e257c938e0a0
Jisu0528 <0528jisu@gmail.com>
2024-05-01 18:11:27 +0900

    feat: 프로필 사진 편집 기능 구현 - EditProfile 파일 생성 - camera 이미지 추가

--- src/components/setting/Account.tsx
+++ src/components/setting/Account.tsx
@@ -1,12 +1,21 @@
 import styled from "styled-components";
+import EditProfile from "./EditProfile";
 
 const Account = () => {
+
+  const handleSave = () => {
+
+  }
+
   return (
     <Wrapper>
-      <Title>
-        안녕하세요, 홍길동 님
-      </Title>
-
+      <Profile>
+        <EditProfile />
+        <Title>
+          안녕하세요, 홍길동 님
+        </Title>          
+      </Profile>
+      <SaveBtn onClick={handleSave}>저장</SaveBtn>
     </Wrapper>
   );
 };
@@ -15,10 +24,31 @@ const Wrapper = styled.div`
   border: 2px solid ${({ theme }) => (theme.colors.white01)};
   border-radius: 5px;
   margin: 43px;
+  padding: 65px 45px;
+  box-sizing: content-box;
 `;
+
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`
 
 const Title = styled.div`
   font-size: 40px;
+  font-family: NotoSansSemiBold;
+`;
+
+const SaveBtn = styled.button`
+  width: 150px;
+  height: 54px;
+  color: #FFFFFF;
+  background-color: #586FC5;
+  font-size: 20px;
+  font-weight: 600;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
 `;
 
 export default Account;
---
+++ src/components/setting/EditProfile.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import Avatar from "../../assets/avatar_woman.png";
+import Camera from "../../assets/camera.png";
+import React, { useState, ChangeEvent } from "react";
+
+const EditProfile = () => {
+  const [selectedImage, setSelectedImage] = useState<string>(Avatar);
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const result = reader.result;
+        if (typeof result === 'string') {
+          setSelectedImage(result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Img src={selectedImage} alt="profile image" />
+      <Edit>
+        <img src={Camera} alt="edit profile" />
+        <InputFile type="file" onChange={handleImageChange} accept="image/*" />
+      </Edit>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 120px;
+  height: 120px;
+`;
+
+const Img = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 60px;
+`;
+
+const Edit = styled.label`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  cursor: pointer;
+`;
+
+const InputFile = styled.input`
+  display: none;
+`;
+
+export default EditProfile;
