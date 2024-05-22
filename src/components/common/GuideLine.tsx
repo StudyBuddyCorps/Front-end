eb20fb443b21f143d5bd968a9dbe894d9d45c8b3
Jisu0528 <0528jisu@gmail.com>
2024-05-22 18:28:10 +0900

    feat: 사용자 안내 UI 구현 (#54) - WebRTC 사용 - 영상위 가이드라인 설정 - 디폴트 설정 `다음` 버튼 클릭 시 일정시간 대기 후 스터디룸으로 이동 - 대기 시간 동안 사용자 안내 컴포넌트 표시 - 테스트를 위한 대기 시간 임의 설정

---
+++ src/components/common/GuideLine.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const Guideline: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    async function setupCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error("Camera setup failed:", error);
+      }
+    }
+
+    setupCamera();
+  }, []);
+
+  return (
+    <Wrapper>
+      <Container>
+        <VideoContainer>
+        <VideoPlayer ref={videoRef} autoPlay />
+          <Overlay>
+            <Rectangle />
+            <Circle />
+          </Overlay>
+        </VideoContainer>
+        <Text>보다 명확한 피드백을 위해<br/>화면에 맞춰 앉아 주세요!</Text>      
+      </Container>
+
+      <Info>잠시 후 스터디룸에 입장합니다.</Info>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.background2};
+  justify-content: space-between;
+  padding: 200px 50px 80px;
+  box-sizing: border-box;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const VideoContainer = styled.div`
+  position: relative;
+  width: 630px;
+  height: 400px;
+  overflow: hidden;
+  border: none;
+  border-radius: 5px;
+`;
+
+const VideoPlayer = styled.video`
+  width: 100%;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  pointer-events: none;
+`;
+
+const Circle = styled.div`
+  width: 240px;
+  height: 240px;
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme.colors.main};
+  position: absolute;
+  left: 50%;
+  top: 20%;
+  transform: translate(-50%, -19%); // 네모 위로 위치 조정
+`;
+
+const Rectangle = styled.div`
+  width: 300px;
+  height: 120px;
+  border: 2px solid ${({ theme }) => theme.colors.main};
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 0); // 중앙 정렬
+`;
+
+const Text = styled.div`
+  color: ${({ theme }) => theme.colors.white02};
+  font-size: 32px;
+  font-family: NotoSansSemiBold;
+  margin-top: 20px;
+  text-align: center;
+`;
+
+const Info = styled.div`
+  color: ${({ theme }) => theme.colors.main};
+  font-size: 24px;
+  font-family: NotoSansMedium;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+export default Guideline;
--- src/pages/StudyRoomSetting.tsx
+++ src/pages/StudyRoomSetting.tsx
@@ -5,9 +5,11 @@ import StudyType from "components/studySetting/StudyType";
 import StudyMate from "components/studySetting/StudyMate";
 import StudyHelper from "components/studySetting/StudyHelper";
 import DefaultSetting from "components/studySetting/DefaultSetting";
+import Guideline from "components/common/GuideLine";
 
 const StudyRoomSetting: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState('1. 스터디룸 타입');
+  const [loading, setLoading] = useState(false);
 
   const handleTabSelect = (tab: string) => {
     setSelectedTab(tab);
@@ -15,17 +17,22 @@ const StudyRoomSetting: React.FC = () => {
 
   return (
     <Wrapper>
-      <SettingTab
-        tabs={['1. 스터디룸 타입', '2. 스터디 메이트', '3. 스터디 도우미', '4. 디폴트 설정']}
-        selectedTab={selectedTab}
-        onSelectTab={handleTabSelect}
-      />
+      { loading ? (
+        <Guideline />
+      ) : (
+        <>
+          <SettingTab
+            tabs={['1. 스터디룸 타입', '2. 스터디 메이트', '3. 스터디 도우미', '4. 디폴트 설정']}
+            selectedTab={selectedTab}
+            onSelectTab={handleTabSelect}
+          />
 
-      {selectedTab === '1. 스터디룸 타입' ? <StudyType setSelectedTab={setSelectedTab} /> 
-      : selectedTab === '2. 스터디 메이트' ? <StudyMate setSelectedTab={setSelectedTab} /> 
-      : selectedTab === '3. 스터디 도우미' ? <StudyHelper setSelectedTab={setSelectedTab} />
-      : <DefaultSetting setSelectedTab={setSelectedTab} />}
-
+          {selectedTab === '1. 스터디룸 타입' ? <StudyType setSelectedTab={setSelectedTab} /> 
+          : selectedTab === '2. 스터디 메이트' ? <StudyMate setSelectedTab={setSelectedTab} /> 
+          : selectedTab === '3. 스터디 도우미' ? <StudyHelper setSelectedTab={setSelectedTab} />
+          : <DefaultSetting setSelectedTab={setSelectedTab} setLoading={setLoading} />}
+        </>
+      )}
     </Wrapper>
   );
 };
