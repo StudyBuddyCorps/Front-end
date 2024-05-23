5dc11d3dfbb4a56fa177b3e73736cae770a6a3ea
Justice <juijeong8324@gmail.com>
2024-05-24 03:20:12 +0900

    style add color and chage font
    Sign-off-by: juijeong8324 <juijeong8324@gmail.com>

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -13,8 +13,17 @@ const colors = {
   black00: "#131211",
   black01: "#181D24",
   black02: "#212E42",
+  black03: "#4A5058",
   background: "#4A5058",
   background2: "#303237",
+  grey: "#ECECEC",
+  grey2: "#CDCDCD",
+  feeback01: "#FF5F76",
+  feeback02: "#FD8755",
+  feeback03: "#8CE27F",
+  feeback04: "#26C7BD",
+  graph01: "#9200FF",
+  graph02: "#0095FF",
 };
 
 export type Colors = typeof colors;
