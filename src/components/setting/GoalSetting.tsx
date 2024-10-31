7f8e6675bf23b74a57cda15331de15245d71e3c9
Jisu0528 <0528jisu@gmail.com>
2024-11-01 03:45:55 +0900

    feat: 목표시간 설정 api 연결

--- src/components/setting/GoalSetting.tsx
+++ src/components/setting/GoalSetting.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import SelectBox from "../common/SelectBox";
 import { useState } from "react";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { getToken } from "../../utils/localStroage";
 
 const hours: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const minutes: number[] = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
@@ -10,10 +13,30 @@ const GoalSetting: React.FC = () => {
   const [selectedHour, setSelectedHour] = useState<number>(0);
   const [selectedMinute, setSelectedMinute] = useState<number>(0);
 
-  const handleSave = () => {
+  const handleSave = async () => {
+    const token = getToken();
+    const id = token ? (jwtDecode<{ id: string }>(token).id) : null;
+
+    if (!id) {
+      alert("로그인이 필요합니다.");
+      return;
+    }
+
     const total = selectedHour * 60 + selectedMinute; // 시간을 분으로 변환 후 합산
     setTotalTime(total);
     console.log("총 공부시간:", total);
+
+    try {
+      await axios.put(
+        "http://localhost:8080/users/goal",
+        { userId: id, goal: total },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      alert("목표 공부 시간이 성공적으로 업데이트되었습니다.");
+    } catch (error) {
+      console.error("목표 공부 시간 업데이트 실패:", error);
+      alert("목표 공부 시간 업데이트에 실패했습니다.");
+    }
   };
 
   const handleHourChange = (value: number) => {
