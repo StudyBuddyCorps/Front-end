9790c7f08268fe6681f75dfe84f502b5fce8ba05
Jisu0528 <0528jisu@gmail.com>
2024-11-01 03:36:50 +0900

    feat: 닉네임 변경 api 연결

--- src/components/setting/Account.tsx
+++ src/components/setting/Account.tsx
@@ -1,18 +1,89 @@
 import styled from "styled-components";
 import EditProfile from "./EditProfile";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { saveToken, getToken, removeToken } from "../../utils/localStroage";
+import {jwtDecode} from "jwt-decode";
+
+interface UserData {
+  name: string;
+  profileUrl: string;
+  goal: number;
+  defaultSettings: {
+    roomType: string;
+    studyMate: {
+      image: string;
+      voice: string;
+    };
+    assistantTone: string;
+    cameraAccess: boolean;
+  };
+}
 
 const Account = () => {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [name, setName] = useState("");
+  const [isDuplicate, setIsDuplicate] = useState<boolean | null>(null);
 
-  const handleSave = () => {
+  const token = getToken();
+  const id = token ? (jwtDecode<{ id: string }>(token).id) : null;
 
-  }
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/users/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        setUserData(response.data);
+        setName(response.data.name);
+      } catch (error) {
+        console.error("사용자 데이터 가져오기 실패:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  useEffect(() => {
+    if (userData) {
+      console.log("userData: ", userData);
+    }
+  }, [userData]);
+
+
+  const handleCheckDuplicate = async () => {
+    try {
+      console.log("변경된 이름: ", name);
+      const response = await axios.post("http://localhost:8080/users/nickname/check", { name, id });
+      setIsDuplicate(response.data.isDuplicate);
+      console.log("중복: ", isDuplicate);
+    } catch (error) {
+      console.error("닉네임 중복 확인 실패:", error);
+    }
+  };
+
+  const handleNicknameChange = async () => {
+    if (!id) return;
+
+    try {
+      await axios.put("http://localhost:8080/users/nickname", { id, name });
+      alert("닉네임이 성공적으로 변경되었습니다.");
+      setUserData((prevData) => prevData ? { ...prevData, name } : null);
+    } catch (error) {
+      console.error("닉네임 변경 실패:", error);
+      alert("닉네임 변경에 실패했습니다.");
+    }
+  };
 
   return (
     <Wrapper>
       <Profile>
-        <EditProfile />
+        <EditProfile/>
         <Title>
-          안녕하세요, 홍길동 님
+          안녕하세요, {userData?.name || "홍길동"} 님
         </Title>          
       </Profile>
       <Edit>
@@ -21,10 +92,15 @@ const Account = () => {
           <Desciption>한글, 영문, 숫자 조합 2 ~ 18자</Desciption>
           <InputDiv>
             <Input 
-              placeholder="홍길동"
+              value={name} onChange={(e) => setName(e.target.value)} placeholder="닉네임 입력"
             />
-            <SaveBtn>중복 확인</SaveBtn>
+            <SaveBtn onClick={handleCheckDuplicate}>중복 확인</SaveBtn>
           </InputDiv>
+          {isDuplicate !== null && (
+            <DuplicateMessage  isDuplicate={isDuplicate}>
+              {isDuplicate ? "이미 사용 중인 닉네임입니다." : "사용 가능한 닉네임입니다."}
+            </DuplicateMessage>
+          )}
         </Container>
         <Container>
           <Label>이메일</Label>
@@ -32,7 +108,7 @@ const Account = () => {
         </Container>
       </Edit>
       <BtnDiv>
-        <SaveBtn onClick={handleSave}>저장</SaveBtn>
+        <SaveBtn onClick={handleNicknameChange}>저장</SaveBtn>
       </BtnDiv>
     </Wrapper>
   );
@@ -128,6 +204,11 @@ const BtnDiv = styled.div`
   display: flex;
   width: 100%;
   justify-content: flex-end;
+`;
+
+const DuplicateMessage = styled.p<{ isDuplicate: boolean }>`
+  font-size: 14px;
+  color: ${({ isDuplicate }) => (isDuplicate ? "red" : "green")};
 `;
 
 export default Account;
