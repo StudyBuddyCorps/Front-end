1cb7d39ccb704cd31751560529cc3cfef21ce8d4
Justice <juijeong8324@gmail.com>
2024-11-29 02:44:15 +0900

    feat: Add time to String util

--- src/utils/timeLine.ts
+++ src/utils/timeLine.ts
@@ -3,13 +3,29 @@ export const timeToString = (time: number): string => {
   const minutes = `${Math.floor(time / 60)}`;
   const getMinutes = `0${parseInt(minutes) % 60}`.slice(-2);
   const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
-  return `${getHours}:${getMinutes}:${getSeconds}`;
+  return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
 
 export const timeToShortString = (time: number): string => {
   const hours = Math.floor(time / 3600); // 시간 계산
   const minutes = Math.floor((time % 3600) / 60); // 분 계산
+  const seconds = Math.floor(time % 60); // 초 계산
 
-  // `{hours}h {minutes}min` 형식으로 반환
-  return `${hours}h ${minutes}min`;
+  if (hours === 0 && minutes === 0) {
+    return `${seconds}sec`;
+  }
+
+  // `hours`가 0일 때는 분과 초만 표시
+  if (hours === 0) {
+    return `${minutes}min ${seconds}sec`;
+  }
+
+  // `hours`, `minutes`, `seconds` 모두 표시
+  return `${hours}h ${minutes}min ${seconds}sec`;
+};
+
+export const getYearMonth = (date: Date): string => {
+  return `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}`;
 };
